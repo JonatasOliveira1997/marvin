7afa4be820e79339d6b7df395739e96564e02863
JonatasOliveira1997 <59872775+JonatasOliveira1997@users.noreply.github.com>
2020-01-16 09:45:11 -0300

    mostrar dpois nomes na tela separados por um "e"

--- fase01/ex02/nomes.js
+++ fase01/ex02/nomes.js
@@ -1,19 +1,3 @@
- /*function mostrarNome() {
-  const nome = "Marvin";
-  console.log(nome);
- }
- mostrarNome()
- 
- function nomeDeVolta() {
-    const name = "o android paranoid";
-    console.log(name)
- }
- 
- nomeDeVolta()*/
- 
- function nomes(nome1, nome2) {
-     //const nome = ("Jhool" + " e " + "Irineu")
-    
- }
- 
-  console.log(nomes("jhool", "cegdcgd"))
+function nomes(nome1, nome2) {
+    return (nome1 + " e " + nome2);
+}
