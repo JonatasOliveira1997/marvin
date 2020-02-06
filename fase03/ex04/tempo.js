d4fce130d41c4dfe39f4e82b321907854d3ee043
JonatasOliveira1997 <59872775+JonatasOliveira1997@users.noreply.github.com>
2020-02-06 09:38:04 -0300

    cap5

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -3,18 +3,16 @@ function tempo(data1, data2, unidade) {
     var d2 = new Date(data2);
     switch(unidade){
         case "dia":
-            var periodo = Math.abs(d1.getTime()-
-            datad2.getTime())/1000/60/60/24;
-            return Math.round(periodo);
+            var periodo = Math.abs(d1.getTime() - d2.getTime())/1000/60/60/24;
+            return Math.round(periodo)
             break;
-        case "mes":
-            var periodo = Math.abs(d1.getTime()-
-            datad2.getTime())/1000/60/60/24/30;
-            return Math.round(periodo);
-            defaul:
-            var periodo = Math.abs(d1.getTime()-
-            datad2.getTime())/1000/60/60/24/30/12;
-            return Math.round(periodo);
-            break;;
+        case "mês":
+            var periodo1 = Math.abs(d1.getTime() - d2.getTime())/1000/60/60/24/30;
+            return Math.round(periodo1)
+            break;
+        case "ano":
+            var periodo2 = Math.abs(d1.getTime() - d2.getTime())/1000/60/60/24/30/12;
+            return Math.round(periodo2)
+            break;
     }
 }
