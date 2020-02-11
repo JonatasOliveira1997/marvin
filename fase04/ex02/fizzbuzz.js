8f3a9fff0877b02dbcc350a3e1597e2c71438937
JonatasOliveira1997 <59872775+JonatasOliveira1997@users.noreply.github.com>
2020-02-11 09:11:39 -0300

    cap3

---
+++ fase04/ex02/fizzbuzz.js
@@ -0,0 +1,18 @@
+function fizzBuzz(num1, num2) {
+    while(num1 <= num2){
+        if(num1 % 3 === 0 && num1 % 5 === 0) {
+            console.log(num1 + " FizzBuzz");
+            num1++;
+        }
+        if(num1 % 3 === 0){
+            console.log(num1 + " Fizz");
+            num1++;
+        }
+        if(num1 % 5 === 0){
+            console.log(num1 + " Buzz");
+            num1++;
+        }
+        console.log(num1);
+        num1++;
+    }
+}
