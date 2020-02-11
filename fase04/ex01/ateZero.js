5d6506d9e227b83e77541614f8e4d63f63ba7d2c
JonatasOliveira1997 <59872775+JonatasOliveira1997@users.noreply.github.com>
2020-02-11 09:43:39 -0300

    cap2

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,15 @@
+function ateZero(num) {
+    var i = 0;
+    if(i <= num){
+        while(i < num){
+        console.log(i);
+        i++;
+        }
+    }
+    if(num <= i){
+        while(i >= num){
+        console.log(num);
+        num++;
+        }
+    }
+}
