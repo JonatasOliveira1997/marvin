6a611aaee61554150217a63e4e3d0b13820a6984
JonatasOliveira1997 <59872775+JonatasOliveira1997@users.noreply.github.com>
2020-02-11 10:33:45 -0300

    cap3

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,4 +1,4 @@
-function fizzBuzz(num1, num2) {
+function fizzbuzz(num1, num2) {
     while(num1 <= num2){
         if(num1 % 3 === 0 && num1 % 5 === 0) {
             console.log(num1 + " FizzBuzz");
